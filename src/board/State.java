9cbf212c41b4e191d97cd2cc9d71ae3f37f01af9
E.Prouty12@gmail.com <E.Prouty12@gmail.com@e4820b3e-8f51-5c01-0fb2-40f9a973692d>
2011-03-22 22:12:18 +0000

    Added moddifications to state

--- src/board/State.java
+++ src/board/State.java
@@ -18,5 +18,28 @@ public class State
 			}
 		}
 	}
+	
+	public static State addPiece(State current, int player, int x){
+		for (int i = 0; i < current.height; i++){
+			if (current.board[x][i] == 0){
+				current.board[x][i] = player;
+				break;
+			}
+		}
+		
+		return current;
+	}
+	
+	public int evaluate(){
+		int best;
+		
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < height; y++){
+			
+			}
+		}
+		
+		return 0;
+	}
 }
 
