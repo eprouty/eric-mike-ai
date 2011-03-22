58a9d9f7133a8f4b68f84cc6ecc2de07878c1d1e
E.Prouty12@gmail.com <E.Prouty12@gmail.com@e4820b3e-8f51-5c01-0fb2-40f9a973692d>
2011-03-22 22:53:23 +0000

    Added the heuristic

--- trunk/src/board/State.java
+++ trunk/src/board/State.java
@@ -35,11 +35,69 @@ public class State
 		
 		for (int x = 0; x < width; x++){
 			for (int y = 0; y < height; y++){
-			
+				switch(board[x][y]){
+				case 1:
+					movesHeuristic(x, y);
+					break;
+				case 0:
+					y = height;
+				}
 			}
 		}
 		
 		return 0;
+	}
+	
+	private int movesHeuristic(int x, int y){
+		int best = 0;
+		for (int dir = 0; dir < 4; dir++){
+			best = Math.max(best, checkDirection(dir, x, y));
+		}
+		return best;
+	}
+	
+	private int checkDirection(int dir, int x, int y){
+		int movesNeeded = ConnectN.numToWin - 1;
+		
+		switch (dir){
+		case 0:
+			for (int i = 1; i < movesNeeded; i++){
+				if (board[x][y+i] == 1){
+					movesNeeded--;
+				} else {
+					break;
+				}
+			}
+			break;
+		case 1:
+			for (int i = 1; i < movesNeeded; i++){
+				if (board[x+i][y+i] == 1){
+					movesNeeded--;
+				} else {
+					break;
+				}
+			}
+			break;
+		case 2:
+			for (int i = 1; i < movesNeeded; i++){
+				if (board[x+i][y] == 1){
+					movesNeeded--;
+				} else {
+					break;
+				}
+			}
+			break;
+		case 3:
+			for (int i = 1; i < movesNeeded; i++){
+				if (board[x+i][y-i] == 1){
+					movesNeeded--;
+				} else {
+					break;
+				}
+			}
+			break;
+		}
+		return movesNeeded;
 	}
 }
 
